e49ff3df8fef6ea45b3c04b92412c2e8149c404f
dandlezzz <danm@workwithopal.com>
2016-04-18 14:41:53 -0700

    shell script fixes

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-echo "set runtimepath+=~/.vim_runtime
+echo "
+set runtimepath+=~/.vim_runtime
 
 source ~/.vim_runtime/vimrcs/basic.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
 source ~/.vim_runtime/vimrcs/extended.vim
-inoremapource ~/.vim_runtime/my_configs.vim
 catch
-endtry" >> ~/.vimrc
+endtry
+
+inoremapource ~/.vim_runtime/my_configs.vim
+" >> ~/.vimrc
